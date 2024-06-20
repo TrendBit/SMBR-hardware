358f0ffec8ebbe967eaad488050310f1d4f35afb
Petr Malanik <Malanik.Petr.gm@gmail.com>
2024-06-20 16:15:22 +0200

    library: add embedded template library using fetch content

--- fw/library/cmake/etl.cmake
+++ fw/library/cmake/etl.cmake
@@ -1,12 +1,31 @@
-add_library(etl INTERFACE)
+Include(FetchContent)
 
-target_compile_features(etl INTERFACE cxx_std_20)
-
-set(CMAKE_C_STANDARD 11)
-set(CMAKE_CXX_STANDARD 20)
-
-target_include_directories(etl INTERFACE
-    etl/include/
+FetchContent_Declare(
+  etl
+  GIT_REPOSITORY https://github.com/ETLCPP/etl
+  GIT_TAG        20.38.15
 )
+
+FetchContent_MakeAvailable(etl)
+
+if(false)
+    add_library(etl INTERFACE)
+
+    target_compile_features(etl INTERFACE cxx_std_20)
+
+    set(GIT_DIR_LOOKUP_POLICY ALLOW_LOOKING_ABOVE_CMAKE_SOURCE_DIR)
+
+    target_include_directories(etl INTERFACE
+        etl/include/etl
+        etl/include/etl/atomic
+        etl/include/etl/deprecated
+        etl/include/etl/experimental
+        etl/include/etl/generators
+        etl/include/etl/mutex
+        etl/include/etl/private
+        etl/include/etl/profiles
+    )
+endif()
+
 
 
